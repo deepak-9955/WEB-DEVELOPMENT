f6f09cf882477ffb94a05b45772d504a7c286471
deepak-9955 <59096075+deepak-9955@users.noreply.github.com>
2023-01-23 19:13:59 +0530

    Add files via upload

---
+++ minor/Joke game/script.js
@@ -0,0 +1,13 @@
+let joke = [
+    "What’s the best thing about Switzerland? I don’t know, but the flag is a big plus.",
+    "I invented a new word: Plagiarism!",
+    "Did you hear about the mathematician who’s afraid of negative numbers? <br>He’ll stop at nothing to avoid them.",
+    "Why do we tell actors to “break a leg? Because every play has a cast.",
+    "Helvetica and Times New Roman walk into a bar 'Get out of here!' shouts the bartender. 'We don’t serve your type.",
+    "Yesterday I saw a guy spill all his Scrabble letters on the road. I asked him What’s the word on the street?",
+    "How many times can you subtract 10 from 100? Once. The next time you would be subtracting 10 from 90.",
+    "These funny Monday<br> Knock! Knock!<br>Q: Who’s there?<br>A: Control Freak.<br>Q: Con…<br>A: Okay, now you say, 'Control Freak who?",
+    "Here are more knock knock ny!<br> Hear about the new restaurant called Karma?<br>There’s no menu: You get what you deserve.",
+    "A woman in labour suddenly shouted, Shouldn’t! Wouldn’t! Couldn’t! Didn’t! Can’t!<br>Don’t worry, said the doctor. Those are just contractions."
+]
+document.write(joke[Math.floor(Math.random()*10)])
